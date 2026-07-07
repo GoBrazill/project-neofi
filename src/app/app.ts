b3b1ce4750f39279056f2c044c42470f7b7dc1f6
GoBrazill <vinicius.daraujos26@gmail.com>
2026-07-06 23:51:59 -0300

    refactor: restructure the project from react to angular

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { HeroSection } from './hero-section/hero-section';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header, HeroSection],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('neofi');
+}
